289e1069d42d04491731e8c0adafab6f1d2c3e39
Robin Khiv <robinkhiv@gmail.com>
2020-02-29 18:33:01 -0800

    added test data and about page

---
+++ resources/react_routes/About.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { aboutMe } from '../store/Store';
+
+export class About extends Component {
+    state = {
+        content: aboutMe
+    };
+
+    renderContent (content)  {
+        const blogContent = [];
+        content.forEach(element => {
+          if(element.type === 'paragraph') blogContent.push(<p class="text-body">{element.content}</p>);
+          else if (element.type === 'img') blogContent.push(<img class={element.size + " img-fluid "} src={element.src} alt={element.alt} /> ); 
+          else if (element.type === 'title') blogContent.push(<h2 class="text-dark">{element.content}</h2> )  
+        })
+        return (
+            <div class="col-lg-12">
+                {blogContent}
+            </div>
+            );
+    }
+
+    render() {
+        const {blogContent} = this.state.content;
+        return (
+            <section className="container">
+                <div class="row">
+                {this.renderContent(blogContent)}
+                </div>
+            </section>
+        )
+    };
+}
+
+export default About
---
+++ resources/store/Store.js
@@ -0,0 +1,35 @@
+export const blogPost = 
+   {
+        title: "Top 5 Favorite San Diego Breweries",
+        blogContent: [
+            {type: "paragraph", content: 'San Diego, a city in Southern California, is home to some of the most breathtaking beaches and delicious seafood. The most southern city in California, its culture is in-between the busy Los Angeles and the laid-back Orange County. It is a perfect vacation destination to feel the ocean breeze and the lively nightlife.'}, 
+            {type:'img', size: 3, alt: 'smiley', src: "https://mouthfuloftravel.files.wordpress.com/2018/05/0305-2018-03078208050009910.jpeg?w=750"},
+            {type: "paragraph", text: '', style: '', content: 'San Diego, a city in Southern California, is home to some of the most breathtaking beaches and delicious seafood. The most southern city in California, its culture is in-between the busy Los Angeles and the laid-back Orange County. It is a perfect vacation destination to feel the ocean breeze and the lively nightlife.'},
+            {type: "paragraph", text: '', style: '', content: 'San Diego, a city in Southern California, is home to some of the most breathtaking beaches and delicious seafood. The most southern city in California, its culture is in-between the busy Los Angeles and the laid-back Orange County. It is a perfect vacation destination to feel the ocean breeze and the lively nightlife.'},
+            {type: "paragraph", text: '', style: '', content: 'San Diego, a city in Southern California, is home to some of the most breathtaking beaches and delicious seafood. The most southern city in California, its culture is in-between the busy Los Angeles and the laid-back Orange County. It is a perfect vacation destination to feel the ocean breeze and the lively nightlife.'},
+            {type: "paragraph", text: '', style: '', content: 'San Diego, a city in Southern California, is home to some of the most breathtaking beaches and delicious seafood. The most southern city in California, its culture is in-between the busy Los Angeles and the laid-back Orange County. It is a perfect vacation destination to feel the ocean breeze and the lively nightlife.'},
+            {type: "paragraph", text: '', style: '', content: 'San Diego, a city in Southern California, is home to some of the most breathtaking beaches and delicious seafood. The most southern city in California, its culture is in-between the busy Los Angeles and the laid-back Orange County. It is a perfect vacation destination to feel the ocean breeze and the lively nightlife.'}
+        ]
+    }
+export const aboutMe = 
+    {
+        title: "who is this girl",
+        blogContent: [
+            {type:'img', size: 3, alt: 'intro', src:"https://mouthfuloftravel.files.wordpress.com/2018/04/1522653943619638.jpg?w=1024&h=633&crop=1"},
+            {type: "title", content: "About me"},
+            {type: "paragraph", content: "Hello, my name is Carolyne… (hi Carolyne). I am new around here. Long time lurker, first-time poster."},
+            {type: "paragraph", content: "I am an accountant by day and a professional Netflixer by night. Truthfully, I am only working full-time so I can fund my dinners and vacations. As a typical accountant, I am an introvert. I like to be with my own thoughts and admire the beauty in my surroundings. Although I do not talk much, I am a great listener and a loyal friend."},
+            {type: "paragraph", content: "When I am not trying new restaurants or traveling, I am listening to music or going to music festivals and concerts. I love music and its ability to bring people together with the same passion and interest."},
+            {type: "title", content: "How this blog came to life"},
+            {type: "paragraph", content: "I can start with the generic, “I created a blog about food and travel because I love food and I love to travel” but I’m not. Don’t get me wrong, I do like all of that, but it is so obvious and not exactly why I started this blog."},
+            {type: "paragraph", content: "I take great joy in planning my trips and spending countless hours researching on various sites. My conversations with people would often lead to talking about their future travels or where I can get the best food. I am always opened to new recommendations and love participating in discussions on where I can get the best _______."},
+            {type: "paragraph", content: "I needed a place to store my itineraries and notes, as well as, my recommendations and details of my experiences. I was tired of sharing Google Drive documents with everyone as it doesn’t consist of any travel photos but mere names of places. I wanted people to see the pictures I took so everyone can have a better understanding of what I am talking about. Through all my research and adventures, I have learned which places are a must see and which I would suggest avoiding and want to share them with the world."},
+            {type: "paragraph", content: "Thus, the idea of a blog was born."},
+            {type: "paragraph", content: "A friend once said to me “Don’t let your dreams, be dreams”. So what the hell, here I am, trying to make a dream come true."},
+            {type: "paragraph", content: "I am working on creating valuable content for my friends and fellow travelers. Since this is still a work in progress, please bear with me. This blog is in no way intended to be works of art, a literature essay…but my candid thoughts. I just want to share my thoughts, experiences, and excitement with everyone in the simplest way possible."},
+            {type: "paragraph", content: "Now, less talk, more food and adventures."},
+            {type: "paragraph", content: "Thank you for your support. I hope to see you again soon!"}
+        ]
+    }
+;
+
