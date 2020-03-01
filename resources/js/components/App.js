75ea195530d5eff7a44bd328f84728b0caf5be4d
Robin Khiv <robinkhiv@gmail.com>
2020-02-29 18:41:24 -0800

    added routes to app component

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Banner from './Banner';
 import Navigation from './Navigation';
+import About from '../../react_routes/About';
+import Blog from '../../react_routes/Blog';
+import Home from '../../react_routes/Home';
 import { BrowserRouter } from 'react-router-dom';
 
 export default class App extends React.Component {
