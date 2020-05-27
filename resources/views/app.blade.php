fdeb406946158ea58fc8b8500baa9f693adc4c1c
Robin Khiv <robinkhiv@gmail.com>
2020-05-27 00:48:15 -0700

    fixed mixed content

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,10 +8,10 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Traveler</title>
         <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/app.css', true) }}" rel="stylesheet">
     </head>
     <body>
         <div id="app-root"></div>
-        <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+        <script src="{{ asset('js/app.js', true) }}" type="text/javascript"></script>
     </body>
     </html>
